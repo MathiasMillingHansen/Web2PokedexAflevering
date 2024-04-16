157b03b427760f1d82038701098ec286f5a7061d
mathiashansen <331199@via.d>
2024-04-16 14:02:28 +0200

    it works

---
+++ pokedex/src/About.js
@@ -0,0 +1,13 @@
+import './styles.css';
+
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <p>This is a very useful about page</p>
+        </div>
+    );
+};
+
+export default About;
--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Pokemon from './Pokemon';
+import Navbar from './Navbar'; // Import Navbar
+import './styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar /> {/* Include Navbar */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/pokemon/:id" element={<Pokemon />} />
+      </Routes>
+    </Router>
   );
 }
 
